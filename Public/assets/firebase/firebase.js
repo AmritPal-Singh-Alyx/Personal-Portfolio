e04a601cce17a072c9120b18fdc40da267e93743
AmritPal Singh <iamritpalsinghh@gmail.com>
2023-02-28 23:19:05 +0530

    Initial Commit

---
+++ Public/assets/firebase/firebase.js
@@ -0,0 +1,17 @@
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQrWWMDxkrZ0njDwlEXrmIF7RJKGMOeg4",
+  authDomain: "portfolio-22fa3.firebaseapp.com",
+  databaseURL: "https://portfolio-22fa3-default-rtdb.firebaseio.com",
+  projectId: "portfolio-22fa3",
+  storageBucket: "portfolio-22fa3.appspot.com",
+  messagingSenderId: "948458442873",
+  appId: "1:948458442873:web:733df683104bc6d663c4a8",
+  measurementId: "G-T79V1DJ7SN"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
